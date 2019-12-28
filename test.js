e0a5b9b916724d3c582e80ab452d8193997cf881
ablin <space6fic@gmail.com>
2019-12-28 23:29:04 +0100

    playing with navbars design and responsiveness

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-$(".search-txt").keyup(function() {
-    if ($(this).val()) {
-        $(".search-btn").css("opacity", 1);
-        $(".search-btn").css("transform", "rotate(0deg)");
-    } else {
-        $(".search-btn").css("opacity", 0);
-        $(".search-btn").css("transform", "rotate(45deg)");
-    }
-})
 
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
