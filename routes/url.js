5aa61c0d2a87804f65b196273691e59891c4f526
ablin <space6fic@gmail.com>
2020-10-07 20:36:44 +0200

    adapting workspace

--- routes/url.js
+++ routes/url.js
@@ -1,52 +1,48 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validUrl = require('valid-url');
-const shortid = require('shortid');
-const config = require('config');
+const validUrl = require("valid-url");
+const shortid = require("shortid");
+require("dotenv").config();
 
-const utils = require('../utils');
-const Url = require('../models/Url');
+const utils = require("../utils");
+const Url = require("../models/Url");
 
-const {validationResult} = require('express-validator');
-const {vUrl} = require('../validators/vUrl');
+const { validationResult } = require("express-validator");
+const { vUrl } = require("../validators/vUrl");
 
 // @route   POST /api/url/shorten
 // @desc    Create short URL from posted long URL /// Send short URL if long URL already exist in DB
-router.post('/shorten', vUrl, async (req, res) => {
-try {
-    let errors = await utils.checkValidationResult(validationResult(req));
-    if (errors.length > 0)
-        throw new Error("Incorrect URL");
+router.post("/shorten", vUrl, async (req, res) => {
+	try {
+		let errors = await utils.checkValidationResult(validationResult(req));
+		if (errors.length > 0) throw new Error("Incorrect URL");
 
-    const { longUrl } = req.body;
-    const baseUrl = config.get('baseURL');
-    const urlCode = shortid.generate();
+		const { longUrl } = req.body;
+		const baseUrl = process.env.BASEURL;
+		const urlCode = shortid.generate();
 
-    if (!validUrl.isWebUri(baseUrl)) 
-        return res.status(401).json({error: true, message: "Invalid base URL"});
+		if (!validUrl.isWebUri(baseUrl)) return res.status(401).json({ error: true, message: "Invalid base URL" });
 
-    if (validUrl.isWebUri(longUrl)) {
-        var [err, url] = await utils.promise(Url.findOne({ longUrl }));
-        if (err)
-            throw new Error("An error occured while looking for your URL");
-        
-        if (url) {
-            return res.status(200).json(url);
-        } else {
-            const shortUrl = baseUrl + '/' + urlCode;
-            let url = new Url({longUrl, shortUrl, urlCode});
+		if (validUrl.isWebUri(longUrl)) {
+			var [err, url] = await utils.promise(Url.findOne({ longUrl }));
+			if (err) throw new Error("An error occured while looking for your URL");
 
-            [err, url] = await utils.promise(url.save());
-            if (err)
-                throw new Error("An error occured while creating your short URL");
+			if (url) {
+				return res.status(200).json(url);
+			} else {
+				const shortUrl = baseUrl + "/" + urlCode;
+				let url = new Url({ longUrl, shortUrl, urlCode });
 
-            return res.status(200).json(url);
-        }
-    } else 
-        return res.status(401).json({error: true, message: "Invalid long URL"});
-} catch (err) {
-    console.error("Shorten api error:", err.message);
-    return res.status(200).json({error: true, message: err.message});
-}})
+				[err, url] = await utils.promise(url.save());
+				if (err) throw new Error("An error occured while creating your short URL");
+
+				return res.status(200).json(url);
+			}
+		} else return res.status(401).json({ error: true, message: "Invalid long URL" });
+	} catch (err) {
+		console.error("Shorten api error:", err.message);
+		return res.status(200).json({ error: true, message: err.message });
+	}
+});
 
 module.exports = router;
