bf2aeba9021bd55ba7b847443d823b42c122c37f
Andreas Blin <ablin@student.42.fr>
2020-10-26 10:29:33 +0100

    saving progress for react typescript refactor, working on shortened result page and api

--- api/routes/url.js
+++ api/routes/url.js
@@ -2,82 +2,97 @@ const express = require("express");
 const router = express.Router();
 const validUrl = require("valid-url");
 const shortid = require("shortid");
+const rp = require("request-promise");
 require("dotenv").config();
 
 const utils = require("../utils");
 const Url = require("../models/Url");
 
 const { validationResult } = require("express-validator");
-const { vUrl } = require("../validators/vUrl");
+const { vUrl, vCode } = require("../validators/vUrl");
 
 // @ POST route for front-end to API, can probably be replaced with ajax call
 router.post("/", vUrl, async (req, res) => {
-  try {
-    let errors = await utils.checkValidationResult(validationResult(req));
-    if (errors.length > 0) throw new Error("Incorrect URL");
+	try {
+		let errors = await utils.checkValidationResult(validationResult(req));
+		if (errors.length > 0) throw new Error("Incorrect URL");
 
-    const { longUrl } = req.body;
-    if (validUrl.isWebUri(longUrl)) {
-      let options = {
-        method: "POST",
-        uri: `${process.env.BASEURL}/api/url/shorten`,
-        body: { longUrl },
-        json: true,
-      };
+		const { longUrl } = req.body;
+		if (validUrl.isWebUri(longUrl)) {
+			let options = {
+				method: "POST",
+				uri: `${process.env.BASEURL}/api/url/shorten`,
+				body: { longUrl },
+				json: true
+			};
 
-      await rp(options)
-        .then(function (urlObj) {
-          return res.status(200).redirect(`/link/${urlObj.urlCode}`);
-        })
-        .catch(function (err) {
-          console.log("An error occured while processing the query", err);
-          //display flash message here
-          return res.status(200).redirect(`/`);
-        });
-    } else throw new Error("Invalid long URL");
-  } catch (err) {
-    console.log("Post URL error:", err);
-    //flash msg and redirect to /
-    return res.status(500).redirect(`/`);
-  }
+			await rp(options)
+				.then(function (urlObj) {
+					return res.status(200).redirect(`/link/${urlObj.urlCode}`);
+				})
+				.catch(function (err) {
+					console.log("An error occured while processing the query", err);
+					//display flash message here
+					return res.status(200).redirect(`/`);
+				});
+		} else throw new Error("Invalid long URL");
+	} catch (err) {
+		console.log("Post URL error:", err);
+		//flash msg and redirect to /
+		return res.status(500).redirect(`/`);
+	}
 });
 
 // @route   POST /api/url/shorten
 // @desc    Create short URL from posted long URL /// Send short URL if long URL already exist in DB
 router.post("/shorten", vUrl, async (req, res) => {
-  try {
-    let errors = await utils.checkValidationResult(validationResult(req));
-    if (errors.length > 0) throw new Error("Incorrect URL");
+	try {
+		let errors = await utils.checkValidationResult(validationResult(req));
+		if (errors.length > 0) throw new Error("Incorrect URL");
 
-    const { longUrl } = req.body;
-    const baseUrl = process.env.HOST;
-    const urlCode = shortid.generate();
+		const { longUrl } = req.body;
+		const baseUrl = process.env.HOST;
+		const urlCode = shortid.generate();
 
-    if (!validUrl.isWebUri("http://" + baseUrl))
-      return res.status(401).json({ error: true, message: "Invalid base URL" });
+		if (!validUrl.isWebUri("http://" + baseUrl)) return res.status(401).json({ error: true, message: "Invalid base URL" });
 
-    if (validUrl.isWebUri(longUrl)) {
-      var [err, url] = await utils.promise(Url.findOne({ longUrl }));
-      if (err) throw new Error("An error occured while looking for your URL");
+		if (validUrl.isWebUri(longUrl)) {
+			var [err, url] = await utils.promise(Url.findOne({ longUrl }));
+			if (err) throw new Error("An error occured while looking for your URL");
 
-      if (url) {
-        return res.status(200).json(url);
-      } else {
-        const shortUrl = baseUrl + "/" + urlCode;
-        let url = new Url({ longUrl, shortUrl, urlCode });
+			if (url) {
+				return res.status(200).json(url);
+			} else {
+				const shortUrl = baseUrl + "/" + urlCode;
+				let url = new Url({ longUrl, shortUrl, urlCode });
 
-        [err, url] = await utils.promise(url.save());
-        if (err)
-          throw new Error("An error occured while creating your short URL");
+				[err, url] = await utils.promise(url.save());
+				if (err) throw new Error("An error occured while creating your short URL");
 
-        return res.status(200).json(url);
-      }
-    } else
-      return res.status(401).json({ error: true, message: "Invalid long URL" });
-  } catch (err) {
-    console.log("Shorten api error:", err.message);
-    return res.status(200).json({ error: true, message: err.message });
-  }
+				return res.status(200).json(url);
+			}
+		} else return res.status(401).json({ error: true, message: "Invalid long URL" });
+	} catch (err) {
+		console.log("Shorten api error:", err.message);
+		return res.status(400).json({ error: true, message: err.message });
+	}
+});
+
+router.get("/:urlCode", vCode, async (req, res) => {
+	try {
+		let errors = await utils.checkValidationResult(validationResult(req));
+		if (errors.length > 0) throw new Error("Incorrect Code");
+
+		let { urlCode } = req.params; //sanitize (verify code look alike a code we could have generated?)
+
+		var [err, url] = await utils.promise(Url.findOne({ urlCode }));
+		if (err) throw new Error("An error occured while looking for your URL");
+		if (url) return res.status(200).json(url);
+		else return res.status(404).json({ error: true, message: "No URL found" });
+	} catch (err) {
+		console.log("Get URL error:", err.message);
+		return res.status(500).json({ error: true, message: err.message });
+	}
 });
 
 module.exports = router;
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,284 +1,340 @@
 import React, { Component, useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  useRouteMatch,
-  useParams,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, useRouteMatch, useParams, Redirect } from "react-router-dom";
 import "./index.css";
 import validUrl from "valid-url";
+import axios from "axios";
 
 function Toggler() {
-  const [state, setState] = useState({ isDarkMode: false });
+	const [state, setState] = useState({ isDarkMode: false });
 
-  useEffect(() => {
-    let darkThemeSelected =
-      localStorage.getItem("themeSwitch") !== null &&
-      localStorage.getItem("themeSwitch") === "dark";
-    let checkbox = document.querySelector(
-      "input[name=theme]"
-    ) as HTMLInputElement;
-    if (checkbox) checkbox.checked = darkThemeSelected;
+	useEffect(() => {
+		let darkThemeSelected = localStorage.getItem("themeSwitch") !== null && localStorage.getItem("themeSwitch") === "dark";
+		let checkbox = document.querySelector("input[name=theme]") as HTMLInputElement;
+		if (checkbox) checkbox.checked = darkThemeSelected;
 
-    darkThemeSelected
-      ? document.documentElement.setAttribute("data-theme", "dark")
-      : document.documentElement.removeAttribute("data-theme");
-  });
+		darkThemeSelected
+			? document.documentElement.setAttribute("data-theme", "dark")
+			: document.documentElement.removeAttribute("data-theme");
+	});
 
-  let transitionTheme = () => {
-    document.documentElement.classList.add("transition");
-    window.setTimeout(() => {
-      document.documentElement.classList.remove("transition");
-    }, 750);
-  };
+	let transitionTheme = () => {
+		document.documentElement.classList.add("transition");
+		window.setTimeout(() => {
+			document.documentElement.classList.remove("transition");
+		}, 750);
+	};
 
-  function handleToggler() {
-    setState({ isDarkMode: !state.isDarkMode });
+	function handleToggler() {
+		setState({ isDarkMode: !state.isDarkMode });
 
-    if (state.isDarkMode) {
-      transitionTheme();
-      localStorage.setItem("themeSwitch", "dark");
-      document.documentElement.setAttribute("data-theme", "dark");
-    } else {
-      transitionTheme();
-      localStorage.removeItem("themeSwitch");
-      document.documentElement.setAttribute("data-theme", "light");
-    }
-  }
+		if (state.isDarkMode) {
+			transitionTheme();
+			localStorage.setItem("themeSwitch", "dark");
+			document.documentElement.setAttribute("data-theme", "dark");
+		} else {
+			transitionTheme();
+			localStorage.removeItem("themeSwitch");
+			document.documentElement.setAttribute("data-theme", "light");
+		}
+	}
 
-  return (
-    <div className="toggle-container">
-      <input
-        onChange={handleToggler}
-        type="checkbox"
-        id="toggler"
-        name="theme"
-      />
-      <label htmlFor="toggler">Toggle</label>
-    </div>
-  );
+	return (
+		<div className="toggle-container">
+			<input onChange={handleToggler} type="checkbox" id="toggler" name="theme" />
+			<label htmlFor="toggler">Toggle</label>
+		</div>
+	);
 }
 
 function ShortenForm() {
-  const [state, setState] = useState({ value: "", errorMsg: "" });
+	const [state, setState] = useState({ value: "", errorMsg: "" });
 
-  function checkErrors() {
-    let errMsg = "";
-    let value = state.value.trim();
+	function checkErrors() {
+		let errMsg = "";
+		let value = state.value.trim();
 
-    // PROPER URL VALIDATION HERE
-    if (!validUrl.isWebUri(value)) errMsg = "URL not properly formatted";
+		// PROPER URL VALIDATION HERE
+		if (!validUrl.isWebUri(value)) errMsg = "URL not properly formatted";
 
-    return errMsg;
-  }
+		return errMsg;
+	}
 
-  function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
-    //can handle input validation here too
+	function handleInput(e: React.ChangeEvent<HTMLInputElement>) {
+		//can handle input validation here too
 
-    let value = e.target.value;
-    let btn = document.querySelector(".search-btn") as HTMLInputElement;
+		let value = e.target.value;
+		let btn = document.querySelector(".search-btn") as HTMLInputElement;
 
-    if (value.length > 0 && btn) {
-      btn.style.opacity = "1";
-      btn.style.transform = "rotate(0deg)";
-    } else {
-      btn.style.opacity = "0";
-      btn.style.transform = "rotate(45deg)";
-    }
+		if (value.length > 0 && btn) {
+			btn.style.opacity = "1";
+			btn.style.transform = "rotate(0deg)";
+		} else {
+			btn.style.opacity = "0";
+			btn.style.transform = "rotate(45deg)";
+		}
 
-    setState({ value: e.target.value, errorMsg: checkErrors() });
-  }
+		setState({ value: e.target.value, errorMsg: checkErrors() });
+	}
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    let value = state.value;
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		let value = state.value;
 
-    /*
-    fetch("/api/url", {
-      method: "POST",
-      body: { value },
-      json: true,
-    });*/
+		axios
+			.post("/api/url/shorten", {
+				longUrl: value
+			})
+			.then(function (response) {
+				window.location.href = "/link/" + response.data.urlCode;
+				//redirect to link and pass data
+				console.log(response);
+			})
+			.catch(function (error) {
+				console.log(error);
+			});
+	}
 
-    console.log("handle form submission/validation here");
-  }
+	return (
+		<>
+			<div className="hero-text">
+				<h1 className="heroAnim">Dead simple URL shortener</h1>
+				<h2 className="heroAnim">Copy and paste your link below</h2>
+			</div>
 
-  return (
-    <>
-      <div className="hero-text">
-        <h1 className="heroAnim">Dead simple URL shortener</h1>
-        <h2 className="heroAnim">Copy and paste your link below</h2>
-      </div>
-
-      <h1>{state.errorMsg}</h1>
-      <div className="form-wrapper">
-        <form
-          method="POST"
-          action="/"
-          onSubmit={handleSubmit}
-          className="shorten-form"
-        >
-          <input
-            onChange={handleInput}
-            name="longUrl"
-            type="text"
-            className="search-txt"
-            placeholder="Your link here..."
-            value={state.value}
-            required
-          />
-          <button type="submit" className="search-btn">
-            <i className="fas fa-arrow fa-arrow-right"></i>
-          </button>
-        </form>
-      </div>
-    </>
-  );
+			<h1>{state.errorMsg}</h1>
+			<div className="form-wrapper">
+				<form method="POST" action="/" onSubmit={handleSubmit} className="shorten-form">
+					<input
+						onChange={handleInput}
+						name="longUrl"
+						type="text"
+						className="search-txt"
+						placeholder="Your link here..."
+						value={state.value}
+						required
+					/>
+					<button type="submit" className="search-btn">
+						<i className="fas fa-arrow fa-arrow-right"></i>
+					</button>
+				</form>
+			</div>
+		</>
+	);
 }
 
 function HomeBtn() {
-  return (
-    <form action="/">
-      <button className="btn-404">
-        <i className="fas fa-home fa-2x" aria-hidden="true"></i>
-      </button>
-    </form>
-  );
+	return (
+		<form action="/">
+			<button className="btn-404">
+				<i className="fas fa-home fa-2x" aria-hidden="true"></i>
+			</button>
+		</form>
+	);
 }
 
 function NotFound() {
-  return (
-    <>
-      <h1 className="not-found">404</h1>
-      <HomeBtn></HomeBtn>
-    </>
-  );
+	return (
+		<>
+			<h1 className="not-found">404</h1>
+			<HomeBtn></HomeBtn>
+		</>
+	);
 }
 
 interface UrlType {
-  id: string;
+	id: string;
 }
 
-function Shortened() {
-  let { id } = useParams<UrlType>();
+interface UrlObject {
+	id: string;
+	longUrl: string;
+	shortUrl: string;
+	urlCode: string;
+	date: string;
+	createdAt: string;
+	updatedAt: string;
+	__v: number;
+}
 
-  function handleClipboard(e: React.MouseEvent<HTMLElement>) {
-    e.preventDefault();
-    let toCopy = document.querySelector("#clipper") as HTMLInputElement;
-    let value = toCopy.value;
-    let tooltip = document.querySelector(".tooltiptext");
+type CardProps = {
+	id: string;
+	longUrl: string;
+	shortUrl: string;
+};
 
-    if (tooltip && !tooltip.classList.contains("tooltip-visible")) {
-      tooltip.classList.add("tooltipAnim");
-      tooltip.classList.add("tooltip-visible");
-      setTimeout(() => {
-        if (tooltip) {
-          tooltip.classList.remove("tooltip-visible");
-          tooltip.classList.remove("tooltipAnim");
-        }
-      }, 1000);
-    }
+//export const Card = ({ title, paragraph }: CardProps) => <aside></aside>
 
-    copyClipboard(value);
-  }
+function Shortened({ id, longUrl, shortUrl }: CardProps) {
+	/*
+	const [state, setState] = useState<UrlObject>({
+		id: "",
+		longUrl: "",
+		shortUrl: "",
+		urlCode: "",
+		date: "",
+		createdAt: "",
+		updatedAt: "",
+		__v: 0
+	});
+	let { id } = useParams<UrlType>();
 
-  function copyClipboard(value: string) {
-    let tempInput = document.createElement("input");
-    tempInput.value = value;
+	useEffect(() => {
+		async function axiosTest() {
+			try {
+				let url = "/api/url/" + id;
+				const response = await axios.get(url);
 
-    document.body.appendChild(tempInput);
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-  }
+				setState(response.data);
+			} catch (err) {
+				//window.location.href = "/";
+				console.log(err);
+			}
+		}
 
-  return (
-    <div className="url-container">
-      <div className="clipboard-wrapper">
-        <div className="tooltip">
-          <h2 data-clipboard="" onClick={handleClipboard}>
-            Copy to clipboard
-          </h2>
-          <a data-clipboard="" onClick={handleClipboard} href="./#">
-            <i className="fas fa-copy"></i>
-          </a>
+		axiosTest();
+	}, [id]);*/
 
-          <span className="tooltiptext">Copied!</span>
-        </div>
-      </div>
-      <a
-        target="_blank"
-        href="URL LINK TO REDIRECT TO"
-        className="shortened-link heroAnim"
-      >
-        xs-url.fr/{id}
-      </a>
-      <input
-        type="hidden"
-        value="http://xs-url.fr/{{dt_plnW_B}}"
-        id="clipper"
-      />
+	function handleClipboard(e: React.MouseEvent<HTMLElement>) {
+		e.preventDefault();
+		let toCopy = document.querySelector("#clipper") as HTMLInputElement;
+		let value = toCopy.value;
+		let tooltip = document.querySelector(".tooltiptext");
 
-      <HomeBtn></HomeBtn>
-    </div>
-  );
+		if (tooltip && !tooltip.classList.contains("tooltip-visible")) {
+			tooltip.classList.add("tooltipAnim");
+			tooltip.classList.add("tooltip-visible");
+			setTimeout(() => {
+				if (tooltip) {
+					tooltip.classList.remove("tooltip-visible");
+					tooltip.classList.remove("tooltipAnim");
+				}
+			}, 1000);
+		}
+
+		copyClipboard(value);
+	}
+
+	function copyClipboard(value: string) {
+		let tempInput = document.createElement("input");
+		tempInput.value = value;
+
+		document.body.appendChild(tempInput);
+		tempInput.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempInput);
+	}
+
+	return (
+		<div className="url-container">
+			<div className="clipboard-wrapper">
+				<div className="tooltip">
+					<h2 data-clipboard="" onClick={handleClipboard}>
+						Copy to clipboard
+					</h2>
+					<a data-clipboard="" onClick={handleClipboard} href="./#">
+						<i className="fas fa-copy"></i>
+					</a>
+
+					<span className="tooltiptext">Copied!</span>
+				</div>
+			</div>
+			<a target="_blank" href={longUrl} rel="noopener noreferrer" className="shortened-link heroAnim">
+				xs-url.fr/{id}
+			</a>
+			<input type="hidden" value={"http://" + shortUrl} id="clipper" />
+
+			<HomeBtn></HomeBtn>
+		</div>
+	);
 }
 
 function ShortenResult() {
-  let match = useRouteMatch();
+	let match = useRouteMatch();
 
-  return (
-    <Switch>
-      <Route exact path={`${match.url}/:id`}>
-        <Shortened />
-      </Route>
-      <Route path={match.path}>
-        <Redirect to="/" />
-      </Route>
-    </Switch>
-  );
+	const [state, setState] = useState<UrlObject>({
+		id: "",
+		longUrl: "",
+		shortUrl: "",
+		urlCode: "",
+		date: "",
+		createdAt: "",
+		updatedAt: "",
+		__v: 0
+	});
+	let { id } = useParams<UrlType>();
+	console.log(id, "ID");
+
+	async function axiosTest() {
+		try {
+			let url = "/api/url/" + id;
+			const response = await axios.get(url);
+
+			setState(response.data);
+		} catch (err) {
+			window.location.href = "/";
+		}
+	}
+
+	useEffect(() => {}, []);
+
+	axiosTest();
+
+	console.log(state.id, state.longUrl, state.shortUrl, "x");
+	if (state.id.length > 0) {
+		console.log(state.id, state.longUrl, state.shortUrl, "z");
+		return (
+			<Switch>
+				<Route exact path={`${match.url}/:id`}>
+					<Shortened id={state.id} longUrl={state.longUrl} shortUrl={state.shortUrl} />
+				</Route>
+				<Route path={match.path}>
+					<Redirect to="/" />
+				</Route>
+			</Switch>
+		);
+	} else {
+		console.log("ifflou");
+		return (
+			<Route path="/">
+				<Redirect to="/" />
+			</Route>
+		);
+	}
 }
 
 interface pageProp {
-  isUrl: Boolean;
+	isUrl: Boolean;
 }
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Toggler></Toggler>
-        <div className="container">
-          <Switch>
-            <Route path="/link/">
-              <ShortenResult></ShortenResult>
-            </Route>
+	render() {
+		return (
+			<Router>
+				<Toggler></Toggler>
+				<div className="container">
+					<Switch>
+						<Route path="/link/">
+							<ShortenResult></ShortenResult>
+						</Route>
 
-            <Route exact path="/">
-              <ShortenForm></ShortenForm>
-            </Route>
+						<Route exact path="/">
+							<ShortenForm></ShortenForm>
+						</Route>
 
-            <Route component={NotFound} />
-          </Switch>
-        </div>
+						<Route component={NotFound} />
+					</Switch>
+				</div>
 
-        <footer className="invert">
-          <p className="footer-text">
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://www.ablin.dev"
-            >
-              @ablin42
-            </a>
-          </p>
-        </footer>
-      </Router>
-    );
-  }
+				<footer className="invert">
+					<p className="footer-text">
+						<a rel="noopener noreferrer" target="_blank" href="https://www.ablin.dev">
+							@ablin42
+						</a>
+					</p>
+				</footer>
+			</Router>
+		);
+	}
 }
 
 export default App;
